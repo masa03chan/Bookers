aec835e45791222e3198e33136042c1db18f4d64
masa03chan <127210098+masa03chan@users.noreply.github.com>
2023-03-18 06:08:36 +0000

    [Add]indexへのshow,edit,destroy表示、routes記入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,10 +19,19 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   
-  def destroy
-    @book = Book.delete
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.)
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  private
   def book_params
     params.require(:book).permit(:title, :body)
   end
